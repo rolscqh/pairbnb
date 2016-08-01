aa849a32a31642700ec5e21ad35b465447174a65
Carol Chan <chanqh@gmail.com>
2016-08-01 14:46:34 +0800

    facebook auth

--- pairbnbapp/app/controllers/users_controller.rb
+++ pairbnbapp/app/controllers/users_controller.rb
@@ -1,3 +1,22 @@
 class UsersController < ApplicationController
 	before_action :require_login
+	has_many :authentications, :dependent => :destroy
+
+  def self.create_with_auth_and_hash(authentication,auth_hash)
+    create! do |u|
+      u.first_name = auth_hash["info"]["first_name"]
+      u.email = auth_hash["extra"]["raw_info"]["email"]
+      u.authentications<<(authentication)
+    end
+  end
+
+  def fb_token
+    x = self.authentications.where(:provider => :facebook).first
+    return x.token unless x.nil?
+  end
+
+  def password_optional?
+    true
+  end
+	
 end
--- pairbnbapp/config/routes.rb
+++ pairbnbapp/config/routes.rb
@@ -11,6 +11,10 @@ Rails.application.routes.draw do
   get "/sign_in" => "clearance/sessions#new", as: "sign_in"
   delete "/sign_out" => "clearance/sessions#destroy", as: "sign_out"
   get "/sign_up" => "clearance/users#new", as: "sign_up"
+
+  get "/auth/:provider/callback" => "sessions#create_from_omniauth"
+
+  resources :users, only: [:show, :edit, :update, :destroy] 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
