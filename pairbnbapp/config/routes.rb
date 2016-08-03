a8789158a7443c25baa1b9c437f35cfe24086b20
Student <student@mxchan-mac.local>
2016-08-03 16:57:12 +0800

    changed landing page

--- pairbnbapp/config/routes.rb
+++ pairbnbapp/config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   resources :passwords, controller: "clearance/passwords", only: [:create, :new]
-  resource :session, controller: "clearance/sessions", only: [:create]
+  resource :session, controller: "sessions", only: [:create]
 
   resources :users, controller: "clearance/users", only: [:create] do
     resource :password,
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   end
 
   get "/sign_in" => "clearance/sessions#new", as: "sign_in"
-  delete "/sign_out" => "clearance/sessions#destroy", as: "sign_out"
+  delete "/sign_out" => "sessions#destroy", as: "sign_out"
   get "/sign_up" => "clearance/users#new", as: "sign_up"
 
   resources :users, only: [:show, :edit, :update, :destroy] 
